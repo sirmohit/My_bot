3c091995d073446e562e44c456d2015e8914d094
unknown <mohitbhade236@example.com>
2024-07-16 16:48:44 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # my_bot
+
+## Prerequisites:
+
+-python 3.7
+-A Telegram Account
+-An OpenAI Account for API key
+
+## How to run code:
---
+++ research/tele_echo_bot.py
@@ -0,0 +1,26 @@
+import logging
+from aiogram import Bot, Dispatcher, executor,types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_TOKEN = os.getenv("TOKEN")
+#print(API_TOKEN)
+
+#config logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot  and dispatcher
+bot = Bot(token = API_TOKEN) 
+dp = Dispatcher(bot)  #it will connet my object with telebot token
+
+@dp.message_handler(commands=['start','help']) 
+async def command_start_handler(message: types.Message):
+    """
+    This handler receives messages with `/start` command or `/help` command
+    """
+
+    await message.reply("Hi\n I am Echo Bot! \n How can I help you.")
+
+if __name__ == "_main_":
+    executor.start_polling(dp, skip_updates=True)
